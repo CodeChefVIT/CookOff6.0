452b5bac7be79ad2e5289ef33780853f6df1d583
Akshat Gupta <33231334+akshatvg@users.noreply.github.com>
2020-08-20 19:26:42 +0530

    date

--- javascript/script.js
+++ javascript/script.js
@@ -47,7 +47,7 @@ const loading = document.getElementById('loading');
 
 const currentYear = new Date().getFullYear();
 
-const newYearTime = new Date(`April 20 ${currentYear + 1} 21:54:00`);
+const newYearTime = new Date(`October 1 ${currentYear + 1} 21:54:00`);
 
 
 
